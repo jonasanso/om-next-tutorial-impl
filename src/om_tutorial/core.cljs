d6b640bbe130be022cfd4dcf439cbc461c496b28
Jonas Anso <jonas.anso@gmail.com>
2015-11-20 15:42:14 +0100

    Your first component

--- src/om_tutorial/core.cljs
+++ src/om_tutorial/core.cljs
@@ -3,6 +3,11 @@
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
 
-(enable-console-print!)
+(defui HelloWorld
+  Object
+  (render [this]
+    (dom/div nil "Hello, world!")))
 
-(println "Hello world!")
+(def hello (om/factory HelloWorld))
+
+(js/ReactDOM.render (hello) (gdom/getElement "app"))

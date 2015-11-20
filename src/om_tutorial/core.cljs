f306e1d7af3e51bc0a44b670fe30f22c3ba36d1e
Jonas Anso <jonas.anso@gmail.com>
2015-11-20 16:04:30 +0100

    Identity and normalization - Something to look at

--- src/om_tutorial/core.cljs
+++ src/om_tutorial/core.cljs
@@ -27,7 +27,24 @@
   [{:keys [state] :as env} key params]
   {:value (get-people state key)})
 
-(def parser (om/parser {:read read}))
+(defmulti mutate om/dispatch)
+
+(defmethod mutate 'points/increment
+  [{:keys [state]} _ {:keys [name]}]
+  {:action
+   (fn []
+     (swap! state update-in
+       [:person/by-name name :points]
+       inc))})
+
+(defmethod mutate 'points/decrement
+  [{:keys [state]} _ {:keys [name]}]
+  {:action
+   (fn []
+     (swap! state update-in
+       [:person/by-name name :points]
+       #(let [n (dec %)] (if (neg? n) 0 n))))})
+
 
 (defui Person
   static om/Ident
@@ -35,12 +52,59 @@
     [:person/by-name name])
   static om/IQuery
   (query [this]
-    '[:name :points]))
+    '[:name :points :age])
+  Object
+  (render [this]
+    (println "Render Person" (-> this om/props :name))
+    (let [{:keys [points name foo] :as props} (om/props this)]
+      (dom/li nil
+        (dom/label nil (str name ", points: " points))
+        (dom/button
+          #js {:onClick
+               (fn [e]
+                 (om/transact! this
+                   `[(points/increment ~props)]))}
+          "+")
+        (dom/button
+          #js {:onClick
+               (fn [e]
+                 (om/transact! this
+                   `[(points/decrement ~props)]))}
+          "-")))))
+
+(def person (om/factory Person {:keyfn :name}))
+
+(defui ListView
+  Object
+  (render [this]
+    (println "Render ListView" (-> this om/path first))
+    (let [list (om/props this)]
+      (apply dom/ul nil
+        (map person list)))))
+
+(def list-view (om/factory ListView))
 
 (defui RootView
   static om/IQuery
   (query [this]
     (let [subquery (om/get-query Person)]
-     `[{:list/one ~subquery} {:list/two ~subquery}])))              
+      `[{:list/one ~subquery} {:list/two ~subquery}]))
+  Object
+  (render [this]
+    (println "Render RootView")
+    (let [{:keys [list/one list/two]} (om/props this)]
+      (apply dom/div nil
+        [(dom/h2 nil "List A")
+         (list-view one)
+         (dom/h2 nil "List B")
+         (list-view two)]))))
+
+(def reconciler
+  (om/reconciler
+    {:state  init-data
+     :parser (om/parser {:read read :mutate mutate})}))
+
+(om/add-root! reconciler
+  RootView (gdom/getElement "app"))           
 
 

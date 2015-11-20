914a7a40fea9c430523d8e1423695efdde308dc9
Jonas Anso <jonas.anso@gmail.com>
2015-11-20 15:48:01 +0100

    Changing queries over time

--- src/om_tutorial/core.cljs
+++ src/om_tutorial/core.cljs
@@ -3,22 +3,48 @@
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
 
-(def app-state (atom {:count 0}))
+(def app-state
+  (atom
+    {:app/title "Animals"
+     :animals/list
+     [[1 "Ant"] [2 "Antelope"] [3 "Bird"] [4 "Cat"] [5 "Dog"]
+      [6 "Lion"] [7 "Mouse"] [8 "Monkey"] [9 "Snake"] [10 "Zebra"]]}))
 
-(defui Counter
+(defmulti read (fn [env key params] key))
+
+(defmethod read :default
+  [{:keys [state] :as env} key params]
+  (let [st @state]
+    (if-let [[_ value] (find st key)]
+      {:value value}
+      {:value :not-found})))
+
+(defmethod read :animals/list
+  [{:keys [state] :as env} key {:keys [start end]}]
+  {:value (subvec (:animals/list @state) start end)})
+
+(defui AnimalsList
+  static om/IQueryParams
+  (params [this]
+    {:start 0 :end 10})
+  static om/IQuery
+  (query [this]
+    '[:app/title (:animals/list {:start ?start :end ?end})])
   Object
   (render [this]
-    (let [{:keys [count]} (om/props this)]
+    (let [{:keys [app/title animals/list]} (om/props this)]
       (dom/div nil
-        (dom/span nil (str "Count: " count))
-        (dom/button
-          #js {:onClick
-               (fn [e]
-                 (swap! app-state update-in [:count] inc))}
-          "Click me!")))))
+        (dom/h2 nil title)
+        (apply dom/ul nil
+          (map
+            (fn [[i name]]
+              (dom/li nil (str i ". " name)))
+            list))))))
 
 (def reconciler
-  (om/reconciler {:state app-state}))
+  (om/reconciler
+    {:state app-state
+     :parser (om/parser {:read read})}))
 
 (om/add-root! reconciler
-  Counter (gdom/getElement "app"))
+  AnimalsList (gdom/getElement "app"))
